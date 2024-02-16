a14c5ea43dce428ae08ebb61fed152161cf8d08e
Patrick Hanes <phanes72@gmail.com>
2024-02-16 06:53:33 -0700

    Added the screen

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+#from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+screen.exitonclick()
